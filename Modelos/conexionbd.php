144c9da2abbbb266fb48fab6ef7c364ac8a76033
AndresZambrano23 <olimac-13@hotmail.com>
2018-11-11 00:35:46 -0500

    Taller 3 PA_Estudiante

--- Modelos/conexionbd.php
+++ Modelos/conexionbd.php
@@ -2,11 +2,11 @@
 class Conexion extends PDO{
 
 
-    public function __constructor(){
+    public function __construct(){
     
     
         try{
-            parent::__constructor('mysql:host=localhost;dbname=tallerbd','root','');
+            parent::__construct('mysql:host=localhost;dbname=tallerbd','root','');
             parent::setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
             } catch(Exeption $ex){
                 echo "Error con la base de datos : ".$ex->getMessage();
--- Modelos/estudiante.php
+++ Modelos/estudiante.php
@@ -14,13 +14,10 @@ class estudiantes{
     function identificacionestudiante($cedula){
         $objBaseDatos= new Conexion();
         $this->est_ced = $cedula;
-        $res=$objBaseDatos->prepare("select est_id, est_nom, est_dir, est_tel, est_ced,pro_nom from estudiante es inner join programa pro on es.pro_id = pro.pro_id where est_ced = '$this->est_ced'");
-        //call PA_Estudiante('','$this->est_ced','','','','','','listar_ws')
+        $res=$objBaseDatos->prepare("call PA_Estudiante('','$this->est_ced','','','','','','listar_ws')");
         $res->execute();
         return $res;
-        echo $res;
         $objBaseDatos=null;
-
     }
 
 }
